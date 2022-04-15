d635c3ca65b78a2a1873f2348d08dd406ee64a89
Rishaba Priyan <rishabapriyan@gmail.com>
2022-04-15 21:52:40 +0530

    added Email functionality

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -11,10 +11,10 @@ function ContactMe() {
 
 		emailjs
 			.sendForm(
-				'YOUR_SERVICE_ID',
-				'YOUR_TEMPLATE_ID',
+				process.env.REACT_APP_EMAILJS_SERVICE_ID,
+				process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
 				form.current,
-				'YOUR_PUBLIC_KEY'
+				process.env.REACT_APP_EMAILJS_PUBLIC_KEY
 			)
 			.then(
 				(result) => {
